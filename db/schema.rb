d7ce69870d4f76fe5fb16ef405340cb811d23ebc
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-16 16:57:40 -0400

    Adds basic user model

---
+++ db/schema.rb
@@ -0,0 +1,9 @@
+ActiveRecord::Schema.define(version: 2021_04_16_205414) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
