f979b2d2feafd4cb030b64cae6d839d37b0195f5
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-17 15:06:29 -0400

    Improves Authentication controller spec

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -10,12 +10,21 @@ RSpec.describe AuthenticationController do
 
       expect(response).to redirect_to('/authentication/failure')
     end
+
+    it "their user information should not be stored in a session" do
+      authenticate("does-not-exist@example.com")
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:auth0]
+
+      post :callback
+
+      expect(session[:user]).to be nil
+    end
   end
 
   describe "when the user does exist in the database" do
     it "they should be redirected to the dashboard page" do
       email = "does-exist@example.com"
-      User.create(email: email)
+      User.create!(email: email)
 
       authenticate(email)
       request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:auth0]
@@ -23,6 +32,19 @@ RSpec.describe AuthenticationController do
       post :callback
 
       expect(response).to redirect_to('/dashboard')
+    end
+
+    it "their user information should be stored in a session" do
+      email = "does-exist@example.com"
+      User.create!(email: email)
+
+      authenticate(email)
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:auth0]
+
+      post :callback
+
+      expect(session[:user]).not_to be nil
+      expect(session[:user][:email]).to eq(email)
     end
   end
 end
