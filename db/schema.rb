e24c5c248fd18c84a2238a4e32d29a8e2fc43cbd
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-17 12:19:52 -0400

    Adds Reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_214946) do
+ActiveRecord::Schema.define(version: 2021_04_17_161409) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "date", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2021_04_16_214946) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "reservations", "users"
 end
