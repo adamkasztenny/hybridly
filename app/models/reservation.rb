5baace986e69a87543f57b57314819cdc48da69b
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-17 19:27:04 -0400

    Adds proper validation message to reservation when the user reserves one twice

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   validates :date, presence: true
 
-  validates_uniqueness_of :user_id, :scope => :date
+  validates_uniqueness_of :user_id, :scope => :date, :message => ->(object, data) do
+    "has already reserved #{object.date}"
+  end
 end
