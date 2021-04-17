25fc558f3352c4e306b1af09bb04642aed4f6152
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-17 15:41:29 -0400

    Adds views for creating a reservation

--- spec/features/reservation_spec.rb
+++ spec/features/reservation_spec.rb
@@ -9,13 +9,22 @@ describe "Reserving a spot in the office", type: :feature do
     click_button 'Login'
   end
 
-  it "allows the user to pick a date to go to the office, if the limit is not reached" do
-    pending
+  it "allows the user to pick a date to go to the office" do
+    click_button 'Reserve time in the office'
+    expect(page).to have_content "New Reservation"
 
+    fill_in 'reservation_date', :with => '2022-01-01'
+    click_on "Create Reservation"
+
+    expect(page).to have_content "Reservation for 2022-01-01 successful!"
+  end
+
+  it "displays an error message if no reservation date is picked" do
     click_button 'Reserve time in the office'
 
-    fill_in 'date', :with => '2022-01-01'
+    click_on "Create Reservation"
 
-    expect(page).to have_content "Reservation for Jan 1st, 2022 successful!"
+    expect(page).not_to have_content "Reservation for 2022-01-01 successful!"
+    expect(page).to have_content "Date can't be blank"
   end
 end
