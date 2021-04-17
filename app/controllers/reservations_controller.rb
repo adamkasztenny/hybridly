7e0415946997313ecd334095bc7623be04ab11c0
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-17 15:38:42 -0400

    Adds reservations controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,25 @@
+class ReservationsController < ApplicationController
+  include Secured
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_parameters)
+    @reservation.user = User.new(session[:user])
+
+    if @reservation.save
+      flash.notice = "Reservation for #{@reservation.date} successful!"
+      redirect_to '/dashboard'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_parameters
+    params.require(:reservation).permit(:date)
+  end
+end
