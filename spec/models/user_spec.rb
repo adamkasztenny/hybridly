7fb245c633d6e930b24c20d16c246a49ed8fdcee
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-17 12:20:04 -0400

    Fixes test name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it "not allow the email to be nil" do
+  it "does not allow the email to be nil" do
     user = User.new
 
     expect(user).not_to be_valid
