4293c76a904d33318a07fc396bbe80fa85a149ad
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-18 13:56:40 -0400

    Adds index to created_at for reservation policies

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_000141) do
+ActiveRecord::Schema.define(version: 2021_04_18_175546) do
 
   create_table "reservation_policies", force: :cascade do |t|
     t.integer "office_limit", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["created_at"], name: "index_reservation_policies_on_created_at"
     t.index ["user_id"], name: "index_reservation_policies_on_user_id"
   end
 
