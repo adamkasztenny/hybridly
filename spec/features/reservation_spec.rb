2444f61298b20171ad6f77e2b8f3e33bcd92d69a
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-18 13:50:24 -0400

    Adds pending spec for reservation failure due to office limit being reached

--- spec/features/reservation_spec.rb
+++ spec/features/reservation_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe "Reserving a spot in the office", type: :feature do
+  let(:user) { User.create!(email: "hybridly@example.com") }
+  let(:other_user) { User.create!(email: "hybridly-other@example.com") }
+
   before :each do
-    user = User.create!(email: "hybridly@example.com")
+    admin_user = User.create!(email: "hybridly-admin@example.com")
+    admin_user.add_role(:admin)
+    reservation_policy = ReservationPolicy.new(office_limit: 1, user: admin_user)
+
     authenticate(user.email)
 
     visit '/'
@@ -41,5 +47,44 @@ describe "Reserving a spot in the office", type: :feature do
 
     expect(page).not_to have_content "Reservation for 2022-01-01 successful!"
     expect(page).to have_content "User has already reserved 2022-01-01"
+  end
+
+  it "does not allow users to reserve a spot if the office limit has been reached for that date" do
+    pending
+
+    click_button 'Reserve time in the office'
+
+    fill_in 'reservation_date', :with => '2022-01-01'
+    click_on "Create Reservation"
+
+    authenticate(other_user.email)
+    visit '/'
+    click_button 'Login'
+
+    click_button 'Reserve time in the office'
+
+    fill_in 'reservation_date', :with => '2022-01-01'
+    click_on "Create Reservation"
+
+    expect(page).not_to have_content "Reservation for 2022-01-01 successful!"
+    expect(page).to have_content "Office limit has been reached for 2022-01-01"
+  end
+
+  it "does not allow users to reserve a spot if the office limit has not been reached for that date" do
+    click_button 'Reserve time in the office'
+
+    fill_in 'reservation_date', :with => '2022-01-01'
+    click_on "Create Reservation"
+    expect(page).to have_content "Reservation for 2022-01-01 successful!"
+
+    authenticate(other_user.email)
+    visit '/'
+    click_button 'Login'
+
+    click_button 'Reserve time in the office'
+
+    fill_in 'reservation_date', :with => '2022-01-02'
+    click_on "Create Reservation"
+    expect(page).to have_content "Reservation for 2022-01-02 successful!"
   end
 end
