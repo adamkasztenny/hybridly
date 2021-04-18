65d70adcabe7cf7f68b687e87d8b37d29a5f6ced
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-18 17:52:24 -0400

    Adds helpers for formatting spots remaining

--- spec/helpers/reservation_helper_spec.rb
+++ spec/helpers/reservation_helper_spec.rb
@@ -1,21 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe ReservationHelper do
-  it "returns a message for no people in the office" do
-    message = format_reservation_count(0)
+  context "Reservation count" do
+    it "returns a message for no people in the office" do
+      message = format_reservation_count(0)
 
-    expect(message).to eq("0 people in the office")
+      expect(message).to eq("0 people in the office")
+    end
+
+    it "returns a message for one person in the office" do
+      message = format_reservation_count(1)
+
+      expect(message).to eq("1 person in the office")
+    end
+
+    it "returns a message for multiple people in the office" do
+      message = format_reservation_count(10)
+
+      expect(message).to eq("10 people in the office")
+    end
   end
 
-  it "returns a message for one person in the office" do
-    message = format_reservation_count(1)
+  context "Spots remaining" do
+    it "returns a message for no spots remaining" do
+      message = format_spots_remaining(0)
 
-    expect(message).to eq("1 person in the office")
-  end
+      expect(message).to eq("No spots remaining for today")
+    end
 
-  it "returns a message for multiple people in the office" do
-    message = format_reservation_count(10)
+    it "returns a message for one person in the office" do
+      message = format_spots_remaining(1)
 
-    expect(message).to eq("10 people in the office")
+      expect(message).to eq("1 spot remaining for today")
+    end
+
+    it "returns a message for multiple people in the office" do
+      message = format_spots_remaining(10)
+
+      expect(message).to eq("10 spots remaining for today")
+    end
   end
 end
