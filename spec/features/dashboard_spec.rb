4a8cfa5755b288d9f492564106fa42800c3b7d21
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-18 16:29:21 -0400

    Adds calendar for showing reservations

---
+++ spec/features/dashboard_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe "Dashboard", type: :feature do
+  let!(:user) { create(:user) }
+
+  before do
+    Timecop.freeze(Time.parse('2022-01-01'))
+  end
+
+  after do
+    Timecop.return
+  end
+
+  it "displays a monthly calendar set to the current month" do
+    login_as(user.email)
+
+    expect(page).to have_content "January 2022"
+    expect(page).to have_content "2022-01-01"
+    expect(page).to have_content "2022-01-31"
+
+    expect(page).to have_content "Mon"
+    expect(page).to have_content "Tue"
+    expect(page).to have_content "Wed"
+    expect(page).to have_content "Thu"
+    expect(page).to have_content "Fri"
+    expect(page).to have_content "Sat"
+    expect(page).to have_content "Sun"
+  end
+
+  it "displays the days of the week" do
+    login_as(user.email)
+
+    expect(page).to have_content "Mon"
+    expect(page).to have_content "Tue"
+    expect(page).to have_content "Wed"
+    expect(page).to have_content "Thu"
+    expect(page).to have_content "Fri"
+    expect(page).to have_content "Sat"
+    expect(page).to have_content "Sun"
+  end
+
+  it "displays how many people are in the office on a given day" do
+    create(:reservation_policy, office_limit: 2)
+    create(:reservation, user: user)
+    create(:reservation, user: create(:user, email: 'hybridly-other@example.com'))
+    create(:reservation, date: Date.parse('2022-01-15'), user: user)
+
+    login_as(user.email)
+
+    expect(page).to have_content "2022-01-01\n2 people in the office"
+    expect(page).to have_content "2022-01-15\n1 person in the office"
+  end
+end
