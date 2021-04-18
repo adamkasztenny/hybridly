970074bd67c9c1fdbda32ef5344d22f01e130118
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-18 19:30:50 -0400

    Updates helper to include a message for when there are no people in the office

--- app/helpers/reservation_helper.rb
+++ app/helpers/reservation_helper.rb
@@ -1,6 +1,8 @@
 module ReservationHelper
   def format_reservation_count(count)
-    if count == 1
+    if count == 0
+      people_count = "No people"
+    elsif count == 1
       people_count = "1 person"
     else
       people_count = "#{count} people"
