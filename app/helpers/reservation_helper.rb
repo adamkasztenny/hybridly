5451061428fb2094ee4d22da49218866cd5d042e
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-18 17:52:24 -0400

    Adds helpers for formatting spots remaining

--- app/helpers/reservation_helper.rb
+++ app/helpers/reservation_helper.rb
@@ -8,4 +8,16 @@ module ReservationHelper
 
     "#{people_count} in the office"
   end
+
+  def format_spots_remaining(spots_remaining)
+    if spots_remaining == 0
+      spots_remaining_count = "No spots"
+    elsif spots_remaining == 1
+      spots_remaining_count = "1 spot"
+    else
+      spots_remaining_count = "#{spots_remaining} spots"
+    end
+
+    "#{spots_remaining_count} remaining for today"
+  end
 end
