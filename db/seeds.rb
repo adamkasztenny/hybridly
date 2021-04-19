363112b5cd979f5823c6216096d69355fb0a4721
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-19 19:10:58 -0400

    Adds a ReservationPolicy to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,13 @@ unless default_user_email.nil? || default_user_email.empty?
   Rails.logger.info "Creating admin user with email #{default_user_email}"
   admin_user = User.create!(:email => default_user_email)
   admin_user.add_role(:admin)
+else
+  email = "hybridly@example.com"
+  Rails.logger.info "Creating default admin user with email #{email}"
+  admin_user = User.create!(:email => email)
+  admin_user.add_role(:admin)
 end
+
+office_limit = 5
+Rails.logger.info "Creating default reservation policy with office limit #{office_limit}"
+ReservationPolicy.create!(user: admin_user, office_limit: office_limit)
