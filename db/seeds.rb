a8bbf1435dcff1f115edbd0200886b24e4db49ed
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-19 19:07:47 -0400

    Makes the default user an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 default_user_email = ENV["DEFAULT_USER_EMAIL"]
 
 unless default_user_email.nil? || default_user_email.empty?
-  Rails.logger.info "Creating user with email #{default_user_email}"
-  User.create!(:email => default_user_email)
-
-  Rails.logger.info "Number of users in database: #{User.count}"
+  Rails.logger.info "Creating admin user with email #{default_user_email}"
+  admin_user = User.create!(:email => default_user_email)
+  admin_user.add_role(:admin)
 end
