597530390057de5a9b00b7cc3781723fcb74a4c5
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-18 20:01:09 -0400

    Improves view reservations test by listing multiple reservations

--- spec/features/view_reservations_spec.rb
+++ spec/features/view_reservations_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe "Dashboard", type: :feature do
   let!(:user) { create(:user) }
-  let!(:reservation_policy) { create(:reservation_policy) }
-  let!(:reservation) { create(:reservation, user: create(:user, email: "other-hybridly@example.com")) }
+  let!(:reservation_policy) { create(:reservation_policy, office_limit: 2) }
+  let!(:reservation) { create(:reservation, user: user) }
+  let!(:other_reservation) { create(:reservation, user: create(:user, email: "other-hybridly@example.com")) }
 
   before do
     Timecop.freeze(Time.parse('2022-01-01'))
@@ -16,10 +17,11 @@ describe "Dashboard", type: :feature do
   it "allows the user to click a day on the calendar and view a reservation" do
     login_as(user.email)
 
-    click_on "1 person in the office"
+    click_on "2 people in the office"
 
     expect(page).to have_content "Reservations for 2022-01-01"
-    expect(page).to have_content "1 person in the office"
+    expect(page).to have_content "2 people in the office"
+    expect(page).to have_content "hybridly@example.com"
     expect(page).to have_content "other-hybridly@example.com"
   end
 end
