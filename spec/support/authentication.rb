b4d18be808a5633d62d0defa4e05e26fc679a0ba
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-18 21:12:31 -0400

    Adds avatar to toolbar

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,6 +1,7 @@
 module Authentication
   def authenticate(email)
-    OmniAuth.config.mock_auth[:auth0] = { 'extra' => { 'raw_info' => { :name => email } } }
+    OmniAuth.config.mock_auth[:auth0] =
+      { 'extra' => { 'raw_info' => { :name => email, :picture => 'http://example.com/img.jpeg' } } }
   end
 
   def login_as(email)
