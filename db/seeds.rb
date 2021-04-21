6db66e03c3f7385e4576a31b122345aa6165de21
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-20 21:33:15 -0400

    Makes seeding idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,24 @@ default_user_email = ENV["DEFAULT_USER_EMAIL"]
 
 unless default_user_email.nil? || default_user_email.empty?
   Rails.logger.info "Creating admin user with email #{default_user_email}"
-  admin_user = User.create!(:email => default_user_email)
+  admin_user = User.find_or_create_by!(:email => default_user_email)
   admin_user.add_role(:admin)
 else
   email = "hybridly@example.com"
   Rails.logger.info "Creating default admin user with email #{email}"
-  admin_user = User.create!(:email => email)
+  admin_user = User.find_or_create_by!(:email => email)
   admin_user.add_role(:admin)
 end
 
 office_limit = 3
 Rails.logger.info "Creating default reservation policy with office limit #{office_limit}"
-ReservationPolicy.create!(user: admin_user, office_limit: office_limit)
+ReservationPolicy.find_or_create_by!(user: admin_user, office_limit: office_limit)
 
 Rails.logger.info "Creating other users"
-violette = User.create!(:email => "violette@example.com")
-nia = User.create!(:email => "nia@example.com")
-caleb = User.create!(:email => "caleb@example.com")
-keisha = User.create!(:email => "keisha@example.com")
+violette = User.find_or_create_by!(:email => "violette@example.com")
+nia = User.find_or_create_by!(:email => "nia@example.com")
+caleb = User.find_or_create_by!(:email => "caleb@example.com")
+keisha = User.find_or_create_by!(:email => "keisha@example.com")
 
 today = Date.today.next_occurring(:monday)
 two_days_from_now = today + 2
@@ -27,15 +27,15 @@ four_days_from_now = today + 4
 next_week = today.next_week(:monday)
 
 Rails.logger.info "Creating reservations"
-Reservation.create!(user: violette, date: today)
-Reservation.create!(user: nia, date: today)
-Reservation.create!(user: caleb, date: today)
+Reservation.find_or_create_by!(user: violette, date: today)
+Reservation.find_or_create_by!(user: nia, date: today)
+Reservation.find_or_create_by!(user: caleb, date: today)
 
-Reservation.create!(user: nia, date: two_days_from_now)
-Reservation.create!(user: keisha, date: two_days_from_now)
+Reservation.find_or_create_by!(user: nia, date: two_days_from_now)
+Reservation.find_or_create_by!(user: keisha, date: two_days_from_now)
 
-Reservation.create!(user: violette, date: four_days_from_now)
-Reservation.create!(user: nia, date: four_days_from_now)
-Reservation.create!(user: caleb, date: four_days_from_now)
+Reservation.find_or_create_by!(user: violette, date: four_days_from_now)
+Reservation.find_or_create_by!(user: nia, date: four_days_from_now)
+Reservation.find_or_create_by!(user: caleb, date: four_days_from_now)
 
-Reservation.create!(user: violette, date: next_week)
+Reservation.find_or_create_by!(user: violette, date: next_week)
