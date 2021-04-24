2b892822305b75aff16098a09eacd6314072cc2c
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-24 13:45:32 -0400

    Adds test to demonstrate there can be more workstations than reservation spots

--- spec/models/workstation_spec.rb
+++ spec/models/workstation_spec.rb
@@ -15,6 +15,16 @@ RSpec.describe Workstation, type: :model do
     expect(workstation).to be_valid
   end
 
+  it "is still valid if the total capacity exceeds the office limit" do
+    ReservationPolicy.create!(office_limit: 10, user: admin_user)
+
+    first_workstation = Workstation.new(location: "HR Office", capacity: 5, user: admin_user)
+    expect(first_workstation).to be_valid
+
+    second_workstation = Workstation.new(location: "Engineering", capacity: 10, user: admin_user)
+    expect(second_workstation).to be_valid
+  end
+
   it "is invalid if the capacity is nil" do
     workstation = Workstation.new(location: "HR Office", user: admin_user)
 
