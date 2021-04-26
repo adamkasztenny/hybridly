73a7cec9cfd3bcf9f62d7e050163d09bd3029e9f
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-26 18:25:20 -0400

    Adds workspace to reservation

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,12 @@ Rails.logger.info "Creating default reservation policy with capacity #{capacity}
 ReservationPolicy.find_or_create_by!(user: admin_user, capacity: capacity)
 
 Rails.logger.info "Creating workspaces"
-Workspace.find_or_create_by!(user: admin_user, location: "Engineering", workspace_type: :desks, capacity: 5)
-Workspace.find_or_create_by!(user: admin_user, location: "HR", workspace_type: :desks, capacity: 5)
-Workspace.find_or_create_by!(user: admin_user, location: "Sales", workspace_type: :desks, capacity: 10)
-Workspace.find_or_create_by!(user: admin_user, location: "Board Room", workspace_type: :meeting_room, capacity: 5)
+engineering = Workspace.find_or_create_by!(user: admin_user, location: "Engineering", workspace_type: :desks,
+                                           capacity: 5)
+hr = Workspace.find_or_create_by!(user: admin_user, location: "HR", workspace_type: :desks, capacity: 5)
+sales = Workspace.find_or_create_by!(user: admin_user, location: "Sales", workspace_type: :desks, capacity: 10)
+board_room = Workspace.find_or_create_by!(user: admin_user, location: "Board Room", workspace_type: :meeting_room,
+                                          capacity: 5)
 
 Rails.logger.info "Creating other users"
 violette = User.find_or_create_by!(:email => "violette@example.com")
@@ -33,15 +35,15 @@ four_days_from_now = today + 4
 next_week = today.next_week(:monday)
 
 Rails.logger.info "Creating reservations"
-Reservation.find_or_create_by!(user: violette, date: today)
-Reservation.find_or_create_by!(user: nia, date: today)
-Reservation.find_or_create_by!(user: caleb, date: today)
+Reservation.find_or_create_by!(user: violette, date: today, workspace: board_room)
+Reservation.find_or_create_by!(user: nia, date: today, workspace: board_room)
+Reservation.find_or_create_by!(user: caleb, date: today, workspace: board_room)
 
 Reservation.find_or_create_by!(user: nia, date: two_days_from_now)
-Reservation.find_or_create_by!(user: keisha, date: two_days_from_now)
+Reservation.find_or_create_by!(user: keisha, date: two_days_from_now, workspace: engineering)
 
-Reservation.find_or_create_by!(user: violette, date: four_days_from_now)
-Reservation.find_or_create_by!(user: nia, date: four_days_from_now)
-Reservation.find_or_create_by!(user: caleb, date: four_days_from_now)
+Reservation.find_or_create_by!(user: violette, date: four_days_from_now, workspace: hr)
+Reservation.find_or_create_by!(user: nia, date: four_days_from_now, workspace: hr)
+Reservation.find_or_create_by!(user: caleb, date: four_days_from_now, workspace: sales)
 
 Reservation.find_or_create_by!(user: violette, date: next_week)
