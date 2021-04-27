c07f4cd4a30c5faa65049bbc110f0b97c12bb89d
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-27 19:55:31 -0400

    Adds pending spec for displaying where others are working

--- spec/features/view_reservations_spec.rb
+++ spec/features/view_reservations_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 describe "Dashboard", type: :feature do
   let!(:user) { create(:user) }
-  let!(:reservation_policy) { create(:reservation_policy, capacity: 2) }
-  let!(:reservation) { create(:reservation, user: user) }
-  let!(:other_reservation) { create(:reservation, user: create(:user, email: "other-hybridly@example.com")) }
+  let!(:reservation_policy) { create(:reservation_policy, capacity: 3) }
+
+  let!(:engineering) { create(:workspace, location: "Engineering", user: reservation_policy.user) }
+  let!(:board_room) { create(:workspace, location: "Board Room", user: reservation_policy.user) }
+
+  let!(:reservation) { create(:reservation, user: user, workspace: engineering) }
+  let!(:second_reservation) {
+    create(:reservation, user: create(:user, email: "second-hybridly@example.com"), workspace: board_room)
+  }
+  let!(:third_reservation) { create(:reservation, user: create(:user, email: "third-hybridly@example.com")) }
 
   before do
     Timecop.freeze(Time.parse('2022-01-01'))
@@ -14,15 +21,34 @@ describe "Dashboard", type: :feature do
     Timecop.return
   end
 
-  it "allows the user to click a day on the calendar and the reservations" do
+  it "allows the user to click a day on the calendar and the see the number of people in the office" do
     login_as(user.email)
 
-    click_on "2 people in the office"
+    click_on "3 people in the office"
 
     expect(page).to have_content "Reservations for 2022-01-01"
-    expect(page).to have_content "2 people in the office:"
+    expect(page).to have_content "3 people in the office:"
+  end
+
+  it "allows the user to click a day on the calendar and the see who has reserved a spot in the office" do
+    login_as(user.email)
+
+    click_on "3 people in the office"
 
     expect(page).to have_content "hybridly@example.com"
-    expect(page).to have_content "other-hybridly@example.com"
+    expect(page).to have_content "second-hybridly@example.com"
+    expect(page).to have_content "third-hybridly@example.com"
+  end
+
+  it "allows the user to click a day on the calendar and the see where people are working" do
+    pending
+
+    login_as(user.email)
+
+    click_on "3 people in the office"
+
+    expect(page).to have_content "hybridly@example.com (Working at a desk in Engineering)"
+    expect(page).to have_content "second-hybridly@example.com (Working in Board Room)"
+    expect(page).to have_content "third-hybridly@example.com"
   end
 end
