4079201aebddd99738dd5718541b8ec7d7026f3b
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-29 18:24:42 -0400

    Creates a service to abstract away insight calculations

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -2,7 +2,7 @@ class InsightsController < ApplicationController
   include Secured
 
   def show
-    @all_reservations_per_day = Reservation.group_by_day(:date).count
+    @all_reservations_per_day = InsightsService.all_reservations_per_day
     @number_of_reservations = @all_reservations_per_day.values.sum
     @average_reservations_per_day = @number_of_reservations / @all_reservations_per_day.size.to_f
     @average_reservations_per_day = @number_of_reservations / @all_reservations_per_day.size.to_f
