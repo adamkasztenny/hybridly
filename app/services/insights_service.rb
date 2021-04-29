0e40d9935dcda08c6bd847a31e32ef8e6b8f38b6
Adam Kasztenny <adamkasztenny@gmail.com>
2021-04-29 18:30:53 -0400

    Moves number of reservations check into the InsightsService

--- app/services/insights_service.rb
+++ app/services/insights_service.rb
@@ -2,4 +2,8 @@ class InsightsService
   def self.all_reservations_per_day
     Reservation.group_by_day(:date).count
   end
+
+  def self.number_of_reservations
+    self.all_reservations_per_day.values.sum
+  end
 end
