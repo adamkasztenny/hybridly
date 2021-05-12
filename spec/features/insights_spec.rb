e32276c927b4edf8f816d187856893a4090a437b
Adam Kasztenny <adamkasztenny@gmail.com>
2021-05-11 21:25:29 -0400

    Adds test for default date range

--- spec/features/insights_spec.rb
+++ spec/features/insights_spec.rb
@@ -11,6 +11,7 @@ describe "Insights", type: :feature do
   let!(:third_reservation) {
     create(:reservation, user: create(:user, email: "third-hybridly@example.com"), date: Time.parse('2022-01-05'))
   }
+  let!(:reservation_for_last_month) { create(:reservation, user: user, date: Time.parse('2021-12-30')) }
 
   before do
     Timecop.freeze(Time.parse('2022-01-01'))
@@ -19,10 +20,6 @@ describe "Insights", type: :feature do
     login_as(user.email)
 
     click_on "Insights"
-
-    fill_in 'start_date', :with => '2022-01-01'
-    fill_in 'end_date', :with => '2022-01-31'
-    click_on "View"
   end
 
   after do
@@ -30,31 +27,65 @@ describe "Insights", type: :feature do
     Capybara.ignore_hidden_elements = true
   end
 
-  it "allows the user to view a line chart with the number of reservations per day" do
-    expect(page).to have_content "Reservations Per Day"
+  context "with a date filter" do
+    before do
+      fill_in 'start_date', :with => '2022-01-01'
+      fill_in 'end_date', :with => '2022-01-31'
+      click_on "View"
+    end
 
-    expect(page).to have_content('new Chartkick["LineChart"]')
+    it "allows the user to view a line chart with the number of reservations per day" do
+      expect(page).to have_content "Reservations Per Day"
 
-    expect(page).to have_content '"2022-01-01",2'
-    expect(page).to have_content '"2022-01-02",0'
-    expect(page).to have_content '"2022-01-03",0'
-    expect(page).to have_content '"2022-01-04",0'
-    expect(page).to have_content '"2022-01-05",1'
-    expect(page).to have_content '"2022-01-15",0'
-    expect(page).to have_content '"2022-01-31",0'
+      expect(page).to have_content('new Chartkick["LineChart"]')
+
+      expect(page).to have_content '"2022-01-01",2'
+      expect(page).to have_content '"2022-01-02",0'
+      expect(page).to have_content '"2022-01-03",0'
+      expect(page).to have_content '"2022-01-04",0'
+      expect(page).to have_content '"2022-01-05",1'
+      expect(page).to have_content '"2022-01-15",0'
+      expect(page).to have_content '"2022-01-31",0'
+    end
+
+    it "allows the user to view the average number of reservations per day" do
+      expect(page).to have_content "Average Reservations Per Day"
+      expect(page).to have_content "0.10"
+    end
+
+    it "allows the user to view a pie chart with the total number of reservations and the total number of" +
+       " reservations available" do
+      expect(page).to have_content "Reservations Used vs Available"
+
+      expect(page).to have_content('new Chartkick["PieChart"]')
+      expect(page).to have_content('"Used",3')
+      expect(page).to have_content('"Available",93')
+    end
   end
 
-  it "allows the user to view the average number of reservations per day" do
-    expect(page).to have_content "Average Reservations Per Day"
-    expect(page).to have_content "0.10"
-  end
+  context "without a date filter" do
+    it "allows the user to view a line chart with the number of reservations per day for the last month" do
+      expect(page).to have_content "Reservations Per Day"
 
-  it "allows the user to view a pie chart with the total number of reservations and the total number of" +
-     " reservations available" do
-    expect(page).to have_content "Reservations Used vs Available"
+      expect(page).to have_content('new Chartkick["LineChart"]')
 
-    expect(page).to have_content('new Chartkick["PieChart"]')
-    expect(page).to have_content('"Used",3')
-    expect(page).to have_content('"Available",93')
+      expect(page).to have_content '"2021-12-01",0'
+      expect(page).to have_content '"2021-12-30",1'
+      expect(page).to have_content '"2022-01-01",2'
+    end
+
+    it "allows the user to view the average number of reservations per day for the last month" do
+      expect(page).to have_content "Average Reservations Per Day"
+      expect(page).to have_content "0.09"
+    end
+
+    it "allows the user to view a pie chart with the total number of reservations and the total number of" +
+       " reservations available for the last month" do
+      expect(page).to have_content "Reservations Used vs Available"
+
+      expect(page).to have_content('new Chartkick["PieChart"]')
+      expect(page).to have_content('"Used",3')
+      expect(page).to have_content('"Available",96')
+    end
   end
 end
