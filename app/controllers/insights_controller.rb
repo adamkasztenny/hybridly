a122f7a9340d4fa946be2d857d65282b28f979ee
Adam Kasztenny <adamkasztenny@gmail.com>
2021-05-11 21:18:52 -0400

    Adds date filter on insights page

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -2,8 +2,8 @@ class InsightsController < ApplicationController
   include Secured
 
   def show
-    start_date = Date.today
-    end_date = Date.today.next_month
+    start_date = params.fetch(:start_date, Date.today.last_month).to_date
+    end_date = params.fetch(:end_date, Date.today).to_date
 
     @all_reservations_per_day = InsightsService.all_reservations_per_day(start_date: start_date, end_date: end_date)
     @number_of_reservations = InsightsService.number_of_reservations(start_date: start_date, end_date: end_date)
