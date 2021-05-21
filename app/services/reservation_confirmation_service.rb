ed1fb8b9eac60a221787d302b05b0f273531c8ea
Adam Kasztenny <adamkasztenny@gmail.com>
2021-05-21 14:02:14 -0400

    Uses base app path in QR code

--- app/services/reservation_confirmation_service.rb
+++ app/services/reservation_confirmation_service.rb
@@ -1,6 +1,6 @@
 class ReservationConfirmationService
-  def self.create_qr_code(reservation)
-    confirmation_url = "/reservations/#{reservation.verification_code}/verify"
+  def self.create_qr_code(reservation, domain)
+    confirmation_url = "#{domain}/reservations/#{reservation.verification_code}/verify"
     RQRCode::QRCode.new(confirmation_url).as_svg(
       color: "000",
       shape_rendering: "crispEdges",
