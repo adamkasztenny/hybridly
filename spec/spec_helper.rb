8a5559e93be9e4b157bc007a8346642cc49f0382
Adam Kasztenny <adamkasztenny@gmail.com>
2021-10-23 15:58:31 -0400

    Lint fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,14 @@ require 'simplecov'
 require 'simplecov-lcov'
 
 SimpleCov.start 'rails' do
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.single_report_path = 'coverage/lcov.info'
 
-      formatter SimpleCov::Formatter::LcovFormatter
+    formatter SimpleCov::Formatter::LcovFormatter
 
-      add_filter %w[version.rb initializer.rb]
-    end
+    add_filter %w[version.rb initializer.rb]
+  end
 end
 
 RSpec.configure do |config|
