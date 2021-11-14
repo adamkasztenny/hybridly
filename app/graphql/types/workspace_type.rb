8feaf73af4a9f7a8887ab1ddd0df27f3990e66ff
Adam Kasztenny <adamkasztenny@gmail.com>
2021-11-13 22:37:30 -0500

    Add a GraphQL enum for Workspace Types

--- app/graphql/types/workspace_type.rb
+++ app/graphql/types/workspace_type.rb
@@ -1,7 +1,12 @@
 module Types
+  class WorkspaceTypeType < Types::BaseEnum
+    value "DESKS", "A workspace with desks", value: "desks"
+    value "MEETING_ROOM", "A meeting room", value: "meeting_room"
+  end
+
   class WorkspaceType < Types::BaseObject
-    field :location, String, null: true
-    field :capacity, Integer, null: true
-    field :workspace_type, String, null: true
+    field :location, String, null: false
+    field :capacity, Integer, null: false
+    field :workspace_type, WorkspaceTypeType, null: false
   end
 end
